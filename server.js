c3664909eb1e1916d01c4be68c7ee6f0f785f706
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-06 22:21:43 +0200

    route connected with sendHeaders controller

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+'use strict';
+
 let express = require('express'),
 app = express(),
-config = require('./config'),
+config = require('./config/config'),
+headersController = require('./controllers/headers.server.controller.js'),
 server;
+
+app.get('/', headersController.sendHeaders);
 
 server = app.listen(config.port);
 console.log('Server listening on:', config.port);

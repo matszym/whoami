248bdbd778b4ec90439a1d3dcccbcf183864a969
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-07 00:00:16 +0200

    Removed unnecesary console.log

--- controllers/headers.server.controller.js
+++ controllers/headers.server.controller.js
@@ -3,7 +3,6 @@ let ip = require('request-ip');
 
 exports.sendHeaders = function(req, res) {
   let userAgent = req.headers["user-agent"];
-  console.log(userAgent);
   res.send({
     ipaddress: ip.getClientIp(req),
     language: req.headers["accept-language"].split(',')[0],

0396807456c8e64f40abb24fb7865202255c7b11
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-06 22:46:57 +0200

    Using package ip for getting ip address

--- controllers/headers.server.controller.js
+++ controllers/headers.server.controller.js
@@ -1,10 +1,13 @@
+'use strict';
+let ip = require('ip');
+
 exports.sendHeaders = function(req, res) {
-  'use strict';
-  console.log(req.headers);
-  let userAgent = req.headers["user-agent"]
+  console.log(req.connection);
+  let userAgent = req.headers["user-agent"];
+
   res.send({
-    ipaddress: req.ip,
+    ipaddress: ip.address(),
     language: req.headers["accept-language"].split(',')[0],
-    software: userAgent.slice(userAgent.indexOf('(') + 1, userAgent.indexOf(')'))
+    software: userAgent.slice(userAgent.indexOf('(') + 10, userAgent.indexOf(')'))
   });
 }
